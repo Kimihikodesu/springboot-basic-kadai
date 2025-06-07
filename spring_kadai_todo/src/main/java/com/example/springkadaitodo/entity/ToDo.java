ceb16561e44fbe8676b54b3e44553a6a8d20f194
Kimihikodesu <kimihiko20040115@gmail.com>
2025-06-07 13:01:08 +0900

    Add files via upload

---
+++ spring_kadai_todo/src/main/java/com/example/springkadaitodo/entity/ToDo.java
@@ -0,0 +1,20 @@
+package com.example.springkadaitodo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="todos")
+
+public class ToDo {
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String title;
+	private String priority;
+	private String status;
+}
+
